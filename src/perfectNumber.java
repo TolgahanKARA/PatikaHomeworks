94c7a43221f4f116a587a9bb63848f3cc3b4ecd9
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-05-23 16:09:24 +0300

    perfectNumber

--- src/perfectNumber.java
+++ src/perfectNumber.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class perfectNumber {
+
+    public static void main(String[] args) {
+
+        int perfectNumber, sum = 0;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        perfectNumber = scn.nextInt();
+
+        for (int i = 1; i <= (perfectNumber-1); i++){
+
+            if (perfectNumber % i == 0){
+
+                sum = sum+i;
+
+            }
+
+        }
+
+        if(perfectNumber == sum)
+            System.out.println(sum + " sayısı bir mükemmel sayıdır");
+        else
+            System.out.println(sum + " sayısı bir mükemmel sayıdır değildir");
+    }
+
 }
