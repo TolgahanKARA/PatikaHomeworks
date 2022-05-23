d0f9f0fbba765009cb6666785e3cd379f1311e70
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-05-23 15:47:39 +0300

    minMaxValue

---
+++ src/minMaxValue.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class minMaxValue {
+
+    public static void main(String[] args) {
+
+        int enterValue, min = 0, max = 0, choiceValue;
+        boolean value = true;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Kaç tane sayı gireceğinizi seçiniz : ");
+        choiceValue = scn.nextInt();
+
+        while (value){
+
+            if (choiceValue > 0){
+
+                for (int i = 1; i <= choiceValue; i++ ){
+
+                    System.out.print(i + ". değerinizi giriniz : ");
+                    enterValue = scn.nextInt();
+
+                    if (enterValue < min){
+
+                        min = enterValue;
+
+                    }
+
+                    if (enterValue > max){
+
+                        max = enterValue;
+
+                    }
+
+                }
+
+                System.out.println("Girilen en büyük değer " + max + " girilen en küçük değer " + min + " sayılarıdır");
+
+                choiceValue=0;
+
+            }else
+                break;
+
+        }
+
+    }
+
+}
