0f0408dda766e0711610a8c912ae1af0d3441104
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-06-12 20:37:26 +0300

    initializeHashLearning

---
+++ src/Hash/Main.java
@@ -0,0 +1,13 @@
+package Hash;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+    }
+
+}
