76eb422ffbd36ca20a09a007ba9f95f66d45b9a1
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-06-12 20:51:43 +0300

    examples

--- src/Hash/Main.java
+++ src/Hash/Main.java
@@ -1,12 +1,40 @@
 package Hash;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
 
         HashSet<Integer> hashSet = new HashSet<>();
+
+        hashSet.add(100);
+        hashSet.add(20);
+        hashSet.add(10);
+        hashSet.add(30);
+        hashSet.add(null);
+
+        System.out.println(hashSet.contains(15));
+        System.out.println(hashSet.size());
+        System.out.println(hashSet.isEmpty());
+        System.out.println(hashSet.add(15));
+        hashSet.remove(10);
+
+        for (Integer number : hashSet){
+
+            System.out.println(number);
+
+        }
+
+        //Iterater collectionlarda gezmeyi sağlayan bir yapı
+        Iterator<Integer> iterator = hashSet.iterator();
+
+        while (iterator.hasNext()){
+
+            System.out.println(iterator.next());
+
+        }
 
     }
 
